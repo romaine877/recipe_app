9d0fe0914921b6b92f2896420049b467ac4f1065
romaine877 <romaine877@gmail.com>
2022-02-02 16:17:30 -0500

    div prop fix

--- Components/Layout.js
+++ Components/Layout.js
@@ -1,27 +1,30 @@
-import Head from 'next/head';
-import React from 'react';
-import Navbar from './Navbar';
+import Head from "next/head";
+import React from "react";
+import Navbar from "./Navbar";
 
 export default function Layout({ children }) {
   return (
-      <div>
-          <div children='flex flex-col justify-between'>
+    <div>
+      <div className="flex flex-col justify-between">
+        <Head>
+          <title>The BEST cookshop Uptown</title>
+          <link rel="icon" href="/favicon.ico" />
 
-          <Head>
-                <title>The BEST cookshop Uptown</title>
-                <link rel="icon" href="/favicon.ico" />
-               
-                    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-                    <meta name="description" content="The BEST cookshop Uptown" />
-                    <meta name='keywords' content='cook, shop, cookshop, uptown, food, jamaica' />
-                    <meta name="author" content="Romaine Halstead" />
-                
-          </Head>
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1.0"
+          />
+          <meta name="description" content="The BEST cookshop Uptown" />
+          <meta
+            name="keywords"
+            content="cook, shop, cookshop, uptown, food, jamaica"
+          />
+          <meta name="author" content="Romaine Halstead" />
+        </Head>
 
-            <Navbar />
-          {children}
-          </div>
-
+        <Navbar />
+        {children}
       </div>
+    </div>
   );
 }
