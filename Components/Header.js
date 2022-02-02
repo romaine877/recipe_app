7b9569d7372591e3a23bdcbabff19c68bfb329a0
romaine877 <romaine877@gmail.com>
2022-02-02 14:44:14 -0500

    test#5

--- Components/Header.js
+++ Components/Header.js
@@ -11,7 +11,7 @@ export default function Header() {
     return (
         <header>
              
-            <div className="container pt-5 mx-auto flex md:flex-row justify-between items-center">
+            <div className="pt-5 mx-auto flex md:flex-row justify-between items-center">
             <Link href="/">
                 <a className="font-bold text-2xl" passref="true">
                    <span className="text-green-600 hover:text-yellow-300 transition ease-in-out duration-1000">UP</span><span className="text-yellow-300 hover:text-green-600">TOWN</span>
@@ -31,7 +31,7 @@ export default function Header() {
                     <a className="mx-2 hover:text-cyan-700 uppercase font-semibold transition ease-in-out duration-400">Contact</a>
                 </Link>
             </nav>
-            <MobileNav/>
+            
             </div>
         </header>
     )
--- Components/Layout.js
+++ Components/Layout.js
@@ -6,7 +6,7 @@ import Header from "./Header";
 export default function Layout({children, title, keywords}) {
     return (
         <div>
-            <div className=" flex flex-col justify-between h-screen backdrop-blur-md">
+           
             <Head>
                 <title>{title}</title>
                 <link rel="icon" href="/favicon.ico" />
@@ -18,16 +18,8 @@ export default function Layout({children, title, keywords}) {
                 {children}
             </main>
             <Footer/>
-            <div className="fixed h-screen w-screen flex justify-center z-40">
-                    <div className="top-0" >
-                        <Image src="/images/under-construction.png"
-                        height={152}
-                        width={250}
-                        alt="Under Construction"
-                        />
-                    </div>
-                </div>
-            </div>
+           
+            
         </div>
     )
 }
